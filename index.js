c16d69e91093b2b2c74e3ba246212040bcc5fc8e
TranVanTienDat <ckim32993@gmail.com>
2023-08-01 17:43:04 +0700

    a

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const KEY = "7a19f77b214041f8a29123748233007";
 const fetchData = async (name) => {
   try {
     const responsive = await axios.get(
-      `${API_WEATHER}/forecast.json?key=${KEY}&q=${name}&days=1&aqi=no&alerts=no`
+      `${API_WEATHER}/forecast.json?key=${KEY}&q=${name}&days=7&aqi=no&alerts=no`
     );
-    console.log(responsive.data);
     return responsive.data;
   } catch (error) {
     console.log(error);
@@ -35,11 +34,20 @@ const HourTemperature = $(".item__temperature");
 const HourImage = $(".item__img");
 const hour = $(".item__time");
 const innerHour = $(".inner__element");
+const elementItem = $(".elemet__item");
+const min = $(".min");
+const max = $(".max");
+
+// next days
+
+const weatherNextDay = $(".weather__day");
+const innerContent = $(".inner__content");
+
 //  handle logic
 
-// Handle get days
-const handleDate = () => {
-  const date = new Date();
+// Handle get days and months
+const handleDate = (arg) => {
+  const date = arg ? new Date(arg) : new Date();
 
   const daysOfWeek = [
     "Sunday",
@@ -74,39 +82,79 @@ const handleDate = () => {
 
 const handleMapHour = (id) => {
   const htmls = id.map((item, index) => {
-    return `<div class="elemet__item" data-index=${index}>
+    const hour = item.time.slice(-5);
+    return `<div class="elemet__item ${
+      hour === getCurrentHour() ? "active" : null
+    }" data-index=${index}>
       <span class="item__temperature">${item.temp_c}</span>
       <img class="item__img" src="${item.condition.icon}"/>
-      <span class="item__time">${item.time.slice(-5)}</span>
+      <span class="item__time">${hour}</span>
     </div>`;
   });
   return htmls;
 };
 
+// handle  get current hour
+const getCurrentHour = (id) => {
+  const date = new Date();
+  const hour = date.getHours();
+  const log = hour < 10 ? `0${hour}:00` : `${hour}:00`;
+  return log;
+};
+
+// handle next day
+const handleNextDay = (id) => {
+  let html = "";
+  for (let i = 1; i < 7; i++) {
+    const forecastNextDay = id.forecastday[i]; // lấy dữ liệu
+    const info = forecastNextDay.day;
+    // console.log(info);
+    const date = handleDate(forecastNextDay.date); // chuyển đổi thời gian
+    html += `<div class="content">
+      <div class="content__weather">
+        <span class="weather__day">${date[0]}</span>
+        <img
+          class="weather__status"
+          src="${info.condition.icon}"
+        />
+        <span class="weather__min-max">
+          <span>${info.mintemp_c}</span>
+          <span>${info.maxtemp_c}</span>
+        </span>
+      </div>
+    </div>`;
+  }
+  innerContent.innerHTML = html;
+};
+
+// handle search
 SearchLocation.addEventListener("click", async function () {
   try {
     const valueSearch = valueInput.value;
     valueInput.value = "";
-    const value = valueSearch === "" ? "hanoi" : valueSearch;
-    const result = await fetchData(value);
+    const result = await fetchData(valueSearch);
+    //  info weather to day
     const current = result.current;
-    const forecast = result.forecast.forecastday[0].hour;
-    locationName.innerHTML = result.location.name;
-    infoTemperature.innerHTML = current.temp_c;
-    infoType.innerHTML = current.condition.text;
-    image.setAttribute("src", current.condition.icon);
-    infoOther[0].innerHTML = `${current.cloud}%`;
-    infoOther[1].innerHTML = current.feelslike_c;
-    infoOther[2].innerHTML = `${current.wind_mph}mph`;
+    const forecast = result.forecast.forecastday[0];
+    locationName.innerHTML = result.location.name; // địa điểm
+    infoTemperature.innerHTML = current.temp_c; // nhiệt độ hiện tại
+    infoType.innerHTML = forecast.day.condition.text; //  miêu tả trạng thái thời tiết
+    image.setAttribute("src", forecast.day.condition.icon); // hình ảnh minh họa thời tiết
+    infoOther[0].innerHTML = `${forecast.day.daily_chance_of_rain}%`; // khả năng mưa
+    infoOther[1].innerHTML = forecast.day.avgtemp_c; // nhiệt độ trung bình
+    infoOther[2].innerHTML = `${forecast.day.maxwind_mph}mph`; // tốc độ gió tối đa
+    min.innerHTML = `Min: ${forecast.day.mintemp_c}`; // nhiệt độ cao nhất
+    max.innerHTML = `Max: ${forecast.day.maxtemp_c}`; //  nhiệt độ thấp nhất
 
-    //  today
-    const date = handleDate();
+    const date = handleDate(); // lấy ra thời gian từ handleDate() (kiểu mảng)
     day.innerHTML = date[0];
     month.innerHTML = `${date[1]}, ${date[2]}`;
-
-    const html = handleMapHour(forecast);
-    console.log(html);
+    const html = handleMapHour(forecast.hour);
     innerHour.innerHTML = html.join("");
+    getCurrentHour(forecast.hour); // xử lí thời gian từ api
+
+    //  next days
+    handleNextDay(result.forecast);
   } catch (error) {
     console.log(error);
   }
